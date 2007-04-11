9d0f74ecd0f47af67dcffc643b3418aa82541b49
Steve Ehrenberg <tehdnite@gmail.com>
2007-04-11 04:31:22 +0000

    added 2 different methods. relative_time_to and relative_time_ago

--- lib/mephisto_relative_time.rb
+++ lib/mephisto_relative_time.rb
@@ -1,6 +1,10 @@
 include ActionView::Helpers::DateHelper
 module MephistoRelativeTime 
-  def relative_time(tag)
-    "#{distance_of_time_in_words_to_now tag.to_date} ago"
+  def relative_time_ago(tag)
+    "#{distance_of_time_in_words_to_now tag.to_date}"
+  end
+
+  def relative_time_to(tag)
+    "#{distance_of_time_in_words Time.now, tag.to_date}"
   end
 end
